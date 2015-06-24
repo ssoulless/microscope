5d287dd23d68c9d53b90bb55d32a20defd700640
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-06-24 18:07:19 -0500

    Refactored postsLists route into a RouteController

--- server/fixtures.js
+++ server/fixtures.js
@@ -49,14 +49,14 @@ Posts.insert({
 });
 
 for (var i = 0; i < 10; i++) {
-Posts.insert({
-title: 'Test post #' + i,
-author: sacha.profile.name,
-userId: sacha._id,
-url: 'http://google.com/?q=test-' + i,
-submitted: new Date(now - i * 3600 * 1000),
-commentsCount: 0
-});
+	Posts.insert({
+		title: 'Test post #' + i,
+		author: sacha.profile.name,
+		userId: sacha._id,
+		url: 'http://google.com/?q=test-' + i,
+		submitted: new Date(now - i * 3600 * 1000),
+		commentsCount: 0
+	});
 }
 
 }
